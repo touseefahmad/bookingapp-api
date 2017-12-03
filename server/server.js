88ade656166e247c8543f8374f763b8170bc9744
touseefahmad <touseefahmad788@gmail.com>
2017-12-04 01:41:35 +0500

    sign up  change and added login

--- server/server.js
+++ server/server.js
@@ -14,19 +14,93 @@ app.use(bodyParser.json());
 
 app.post('/signup',(req,res)=>{
 
-  var user =new User({
-    email : req.body.email,
-    reg_no: req.body.reg_no,
-    password: req.body.password
-  });
-
-  user.save().then((user)=>{
+User.find({reg_no: req.body.reg_no}).then((user)=>{
+  console.log('User Length:',user.length);
+  if(user.length>0){
+    console.log('user already existed');
     res.send({
-      code: '200',
-      user
+      code:'400',
+      user:{
+        email:'null',
+        reg_no:'Already registerd',
+        password:'null'
+      }
     });
+  }else{
+    console.log('Saving user');
+    var user =new User({
+      email : req.body.email,
+      reg_no: req.body.reg_no,
+      password: req.body.password
+    });
+    user.save().then((user)=>{
+      res.send({
+        code: '200',
+        user
+      });
+    },(e)=>{
+      console.log('Error while saving:',e);
+      res.send({
+        code:'400',
+        user:{
+          email:'null',
+          reg_no:'null',
+          password:'null'
+        }
+      });
+    });
+  }
+
+},(e)=>{
+  res.send({
+    code:'400',
+    user:{
+      email:'null',
+      reg_no:'null',
+      password:'null'
+    }
+  });
+});
+});
+
+
+//login User
+app.post('/login',(req,res)=>{
+  User.findOne({reg_no:req.body.reg_no}).then((user)=>{
+    console.log('User Returned:',user);
+    if(user){
+      if(user.password === req.body.password){
+        console.log(`userPassword: ${user.password}, passedPassword: ${req.body.password}`);
+        res.send({
+          code: '200',
+          user
+        });
+      }else{
+        console.log('password not matched');
+        res.send({
+          code:'400',
+          user:{
+            email:'null',
+            reg_no:'null',
+            password:'null'
+          }
+      });
+      }
+
+    }else{
+      console.log('user not found');
+      res.send({
+        code:'400',
+        user:{
+          email:'null',
+          reg_no:'null',
+          password:'null'
+        }
+    });
+    }
+
   },(e)=>{
-    console.log('Error:',e);
+    console.log('reg no error',e);
     res.send({
       code:'400',
       user:{
@@ -34,12 +108,11 @@ app.post('/signup',(req,res)=>{
         reg_no:'null',
         password:'null'
       }
-    });
   });
-
+});
 });
 
 
 app.listen(port,()=>{
   console.log('Listening to port:',port);
-})
+});
