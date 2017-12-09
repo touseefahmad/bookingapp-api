61b0ca8df28fd4d119dea54a5faf9aa5b6cd4a7a
touseefahmad <touseefahmad788@gmail.com>
2017-12-09 19:54:15 +0500

    added heroku mongolab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/bookingapp-db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookingapp-db');
 
 module.exports={
   mongoose
